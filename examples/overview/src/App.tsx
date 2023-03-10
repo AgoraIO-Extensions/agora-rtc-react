da59dfcc20d3232641bfa1cda16235258e7cda8d
hyrious <hyrious@outlook.com>
2023-03-10 15:50:00 +0800

    chore: update example

--- examples/overview/src/App.tsx
+++ examples/overview/src/App.tsx
@@ -1,5 +1,37 @@
+import AgoraRTC from "agora-rtc-sdk-ng";
+import { LocalAudioTrack, LocalVideoTrack } from "agora-rtc-react";
+import { useMemo, useState } from "react";
+
+AgoraRTC.setLogLevel(/* warning */ 2);
+
 export const App = () => {
-  return null;
+  const [mic, setMic] = useState(false);
+  const [camera, setCamera] = useState(false);
+
+  const audioTrack = useMemo(
+    () => (mic ? AgoraRTC.createMicrophoneAudioTrack({ ANS: true, AEC: true }) : null),
+    [mic],
+  );
+  const videoTrack = useMemo(() => (camera ? AgoraRTC.createCameraVideoTrack() : null), [camera]);
+
+  return (
+    <>
+      <div>
+        <label>
+          <input type="checkbox" checked={mic} onChange={e => setMic(e.target.checked)} />
+          <span>Microphone</span>
+        </label>
+        <label>
+          <input type="checkbox" checked={camera} onChange={e => setCamera(e.target.checked)} />
+          <span>Camera</span>
+        </label>
+      </div>
+      <div className="tracks">
+        {mic && <LocalAudioTrack track={audioTrack} />}
+        {camera && <LocalVideoTrack track={videoTrack} width={320} height={180} play />}
+      </div>
+    </>
+  );
 };
 
 export default App;
