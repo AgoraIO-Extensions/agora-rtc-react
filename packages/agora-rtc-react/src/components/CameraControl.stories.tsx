e124b381fc286d328c8bc1a98628cea7e3f8b95c
hyrious <hyrious@outlook.com>
2023-03-15 19:00:45 +0800

    chore: add description usage example

--- packages/agora-rtc-react/src/components/CameraControl.stories.tsx
+++ packages/agora-rtc-react/src/components/CameraControl.stories.tsx
@@ -8,6 +8,12 @@ const meta: Meta<CameraControlProps> = {
   component: CameraControl,
   parameters: {
     backgrounds: { default: "light" },
+    docs: {
+      description: {
+        component: "Applied to the title.",
+        story: "Applied to all stories in this file.",
+      },
+    },
   },
 };
 
@@ -16,6 +22,13 @@ export default meta;
 export const CameraOn: StoryObj<CameraControlProps> = {
   args: {
     cameraOn: true,
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: "I have a custom description.",
+      },
+    },
   },
 };
 
