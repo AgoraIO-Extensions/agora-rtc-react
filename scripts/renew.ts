c9af13a59fd5089b60c100ba4b05eabe3abe9c17
crimx <straybugs@gmail.com>
2023-03-23 17:13:54 +0800

    chore(project): update token renew

--- scripts/renew.ts
+++ scripts/renew.ts
@@ -1,59 +1,85 @@
 // https://docs.agora.io/en/video-calling/develop/integrate-token-generation?platform=web
-import dotenv from "dotenv";
-
-dotenv.config({ path: "./.env.local" });
 
 import AgoraToken from "agora-token";
+import dotenv from "dotenv";
+import minimist from "minimist";
+import path from "node:path";
+import fs from "node:fs";
 
 const { RtcRole, RtcTokenBuilder } = AgoraToken;
+const argv = minimist(process.argv.slice(2));
 
-const { AGORA_APPID, AGORA_CERTIFICATE, AGORA_CHANNEL } = process.env;
-if (!AGORA_APPID || !AGORA_CERTIFICATE || !AGORA_CHANNEL) {
-  throw new Error("Missing env AGORA_APPID, AGORA_CERTIFICATE, AGORA_CHANNEL");
+if (argv.write) {
+  main();
 }
 
-const uid = 0;
-const role = RtcRole.PUBLISHER;
-const expiration = 24 * 3600;
+function main() {
+  const rootEnv = loadEnv(path.join(__dirname, ".."));
+  const pkgEnv = loadEnv(process.cwd());
 
-const AGORA_TOKEN = RtcTokenBuilder.buildTokenWithUid(
-  AGORA_APPID,
-  AGORA_CERTIFICATE,
-  AGORA_CHANNEL,
-  uid,
-  role,
-  expiration,
-  expiration,
-);
+  const secrets = loadSecrets(rootEnv, pkgEnv);
+  if (!secrets) return;
 
-import fs from "fs";
-import fg from "fast-glob";
+  const { AGORA_APPID, AGORA_CERTIFICATE } = secrets;
 
-const files = await fg("**/.env.local", {
-  ignore: ["**/node_modules/**"],
-});
+  const AGORA_CHANNEL = pkgEnv.AGORA_CHANNEL || rootEnv.AGORA_CHANNEL || "test";
 
-const envNew = { AGORA_APPID, AGORA_CHANNEL, AGORA_TOKEN };
+  const AGORA_TOKEN = buildToken(AGORA_APPID, AGORA_CERTIFICATE, AGORA_CHANNEL);
 
-for (const file of files) {
-  const envOld = dotenv.parse(fs.readFileSync(file));
-  const mergedEnv = { ...envOld, ...envNew };
+  writeEnv(process.cwd(), { ...pkgEnv, AGORA_APPID, AGORA_CHANNEL, AGORA_TOKEN });
+}
+
+function loadSecrets(rootEnv: dotenv.DotenvParseOutput, pkgEnv: dotenv.DotenvParseOutput) {
+  const pkgEnvConfigured = Boolean(pkgEnv.AGORA_APPID && pkgEnv.AGORA_CERTIFICATE);
+
+  const AGORA_CERTIFICATE = pkgEnvConfigured ? pkgEnv.AGORA_CERTIFICATE : rootEnv.AGORA_CERTIFICATE;
+  if (!AGORA_CERTIFICATE) {
+    console.log("Missing env AGORA_CERTIFICATE. Token renew skipped.");
+    return;
+  }
+
+  const AGORA_APPID = pkgEnvConfigured ? pkgEnv.AGORA_APPID : rootEnv.AGORA_APPID;
+  if (!AGORA_APPID) {
+    console.log("Missing env AGORA_APPID. Token renew skipped.");
+    return;
+  }
+
+  return { AGORA_APPID, AGORA_CERTIFICATE };
+}
+
+function buildToken(AGORA_APPID: string, AGORA_CERTIFICATE: string, AGORA_CHANNEL: string): string {
+  const uid = 0;
+  const role = RtcRole.PUBLISHER;
+  const expiration = 24 * 3600;
+
+  const AGORA_TOKEN = RtcTokenBuilder.buildTokenWithUid(
+    AGORA_APPID,
+    AGORA_CERTIFICATE,
+    AGORA_CHANNEL,
+    uid,
+    role,
+    expiration,
+    expiration,
+  );
+
+  return AGORA_TOKEN;
+}
+
+function loadEnv(envDir: string) {
+  try {
+    return dotenv.parse(fs.readFileSync(path.join(envDir, ".env.local")));
+  } catch {
+    return {};
+  }
+}
+
+function writeEnv(envDir: string, data: Record<string, string>) {
+  const file = path.join(envDir, ".env.local");
   fs.writeFileSync(
     file,
-    Object.entries(mergedEnv)
+    Object.entries(data)
       .map(([k, v]) => `${k}=${v}\n`)
       .join(""),
   );
-  console.log("updated", file);
-}
-
-// ensure root .env.local
-if (!files.includes(".env.local")) {
-  fs.writeFileSync(
-    ".env.local",
-    Object.entries(envNew)
-      .map(([k, v]) => `${k}=${v}\n`)
-      .join(""),
-  );
-  console.log("created", ".env.local");
+  console.log("Updated", file);
 }
