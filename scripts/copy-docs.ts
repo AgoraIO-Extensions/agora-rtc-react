56cbc92e5555ecc940a7eb6c3bd47e47545d06ff
crimx <straybugs@gmail.com>
2023-03-28 17:40:49 +0800

    chore(project): copy exmaples and storybook to docs

---
+++ scripts/copy-docs.ts
@@ -0,0 +1,24 @@
+import path from "node:path";
+import fs from "node:fs";
+
+const docsPath = path.join(__dirname, "..", "docs");
+
+if (fs.existsSync(docsPath)) {
+  fs.rmSync(docsPath, { recursive: true, force: true });
+} else {
+  fs.mkdirSync(docsPath);
+}
+
+const storybookPath = path.join(__dirname, "..", "packages", "agora-rtc-react", "storybook-static");
+if (fs.existsSync(storybookPath)) {
+  fs.cpSync(storybookPath, docsPath, { recursive: true });
+}
+
+for (const exampleName of fs.readdirSync("examples")) {
+  if (exampleName.startsWith(".")) continue;
+
+  const examplePath = path.join("examples", exampleName, "dist");
+  if (fs.existsSync(examplePath) && fs.lstatSync(examplePath).isDirectory()) {
+    fs.cpSync(examplePath, path.join(docsPath, exampleName), { recursive: true });
+  }
+}
