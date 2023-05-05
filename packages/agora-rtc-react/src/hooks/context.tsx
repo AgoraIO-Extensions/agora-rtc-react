1b6c9b05e824292a1373786d06d9a8e911c3f5d7
hyrious <hyrious@outlook.com>
2023-05-05 16:46:11 +0800

    feat: add AgoraRTCScreenShareProvider

--- packages/agora-rtc-react/src/hooks/context.tsx
+++ packages/agora-rtc-react/src/hooks/context.tsx
@@ -39,3 +39,30 @@ export function useRTCClient(client?: IAgoraRTCClient | null): IAgoraRTCClient {
 
   return resolvedClient;
 }
+
+const AgoraRTCScreenShareContext = /* @__PURE__ */ createContext<IAgoraRTCClient | null>(null);
+
+export interface AgoraRTCScreenShareProviderProps {
+  readonly client: IAgoraRTCClient;
+  readonly children?: ReactNode;
+}
+
+export function AgoraRTCScreenShareProvider({
+  client,
+  children,
+}: AgoraRTCScreenShareProviderProps) {
+  return (
+    <AgoraRTCScreenShareContext.Provider value={client}>
+      {children}
+    </AgoraRTCScreenShareContext.Provider>
+  );
+}
+
+/**
+ * Get a screen share client from context.
+ * @param client If a client is provided, it will be used instead.
+ */
+export function useRTCScreenShareClient(client?: IAgoraRTCClient | null): IAgoraRTCClient | null {
+  const clientFromContext = useContext(AgoraRTCScreenShareContext);
+  return client || clientFromContext;
+}
