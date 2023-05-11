6c79c1be03f8adb4ef70d3dd391188155a4ab990
guoxianzhe <53285945+guoxianzhe@users.noreply.github.com>
2023-05-11 18:58:59 +0800

    add gitleak (#9)
    * feat: add gitleak in pre-commit
    
    * feat: add gitleak ci
    
    * fix: remove sensitive code style

--- examples/advanced/src/utils/const.ts
+++ examples/advanced/src/utils/const.ts
@@ -1,6 +1,15 @@
+import CryptoJS from "crypto-js";
+
+let appId = import.meta.env.AGORA_APPID;
+if (import.meta.env.AGORA_AES_SALT) {
+  // only for github-pages demo
+  const bytes = CryptoJS.AES.decrypt(import.meta.env.AGORA_APPID, import.meta.env.AGORA_AES_SALT);
+  appId = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+}
+
 const appConfig = {
-  appId: import.meta.env.AGORA_APPID,
-  channel: import.meta.env.AGORA_CHANNEL,
+  appId: appId,
+  channel: import.meta.env.AGORA_CHANNEL || "test",
   token: import.meta.env.AGORA_TOKEN ? import.meta.env.AGORA_TOKEN : null,
 };
 
