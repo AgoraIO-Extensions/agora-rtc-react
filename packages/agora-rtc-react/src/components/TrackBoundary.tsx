8c1397c8e8540e4ece2bc57b3416b26f8bd0232f
gxz <guoxianzhe@agora.io>
2023-06-05 16:20:05 +0800

    fix: when play is false should stop track directly

--- packages/agora-rtc-react/src/components/TrackBoundary.tsx
+++ packages/agora-rtc-react/src/components/TrackBoundary.tsx
@@ -108,6 +108,8 @@ export function useAutoPlayVideoTrack(
     if (track) {
       if (div && play) {
         track.play(div);
+      } else {
+        track.stop();
       }
 
       if (controller) {
@@ -138,6 +140,8 @@ export function useAutoPlayAudioTrack(
     if (track) {
       if (play) {
         track.play();
+      } else {
+        track.stop();
       }
 
       if (controller) {
