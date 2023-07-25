0e03743641ee7bf87596c93156218ac4b1a40098
guoxianzhe <guoxianzhe@users.noreply.github.com>
2023-07-25 07:07:15 +0000

    deploy: 008c38ad048c8e10024156161fe53d8bbc9dff22

---
+++ assets/preview-b947cb10.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-054b5933.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-EYKKDMVH-770aa357.js"),["./DocsRenderer-EYKKDMVH-770aa357.js","./iframe-054b5933.js","./index-8db94870.js","./_commonjsHelpers-042e6b4d.js","./react-18-f58e807b.js","./index-80ae7d84.js","./index-731bcbf7.js","./index-d475d2ea.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-b947cb10.js.map
