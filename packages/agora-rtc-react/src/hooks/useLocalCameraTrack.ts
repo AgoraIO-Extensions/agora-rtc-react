c3e7583851c095e75014b216adf326a5558e244c
EkaanshArora <ekaansh68@gmail.com>
2023-10-30 18:47:09 +0530

    docs: add track duplicate caveat

--- packages/agora-rtc-react/src/hooks/useLocalCameraTrack.ts
+++ packages/agora-rtc-react/src/hooks/useLocalCameraTrack.ts
@@ -16,6 +16,7 @@ import { useIsConnected } from "./useIsConnected";
  * This hook lets you create a local camera video track.
  * This hook can only create the video track once before the component is destroyed.
  * After the component is unmounted, the video track created by this hook stops publishing.
+ * You can call this method multiple times to create multiple tracks. If you need to access the same track in multiple components, you can pass the same track object to multiple components.
  *
  * @param ready - Whether it is ready to create the track. The default value is `true`.
  * @param cameraVideoTrackConfig - Configurations for initializing the camera video track. See [`CameraVideoTrackInitConfig`](https://api-ref.agora.io/en/video-sdk/web/4.x/interfaces/cameravideotrackinitconfig.html) for details.
